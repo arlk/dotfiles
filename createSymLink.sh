9ca6d33f5ca33c720303f83b86848a3b722b16d4
arunlakshmanan <lakshma2@illinois.edu>
2016-10-16 20:39:34 -0500

    Adding agignore

--- createSymLink.sh
+++ createSymLink.sh
@@ -5,10 +5,10 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 bak=~/backup-dotfiles
 
-dotfiles="zshrc bashrc gitconfig"
+dotfiles="zshrc bashrc gitconfig agignore"
 folders="nvim"
 
-read -r -p "Symlink .zshrc, .bashrc, .gitconfig? [y/N] " response
+read -r -p "Symlink .zshrc, .bashrc, .gitconfig, .agignore? [y/N] " response
 response=${response,,}
 if [[ $response =~ ^(yes|y)$ ]]; then
   echo "Storing dot files at $bak ..."
