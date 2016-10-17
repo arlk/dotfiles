6618b8b42c7e994764710445a622e5de22c7168c
lakshma2 <lakshma2@illinois.edu>
2016-10-16 20:49:56 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -21,23 +21,23 @@ response=${response,,}
 if [[ $response =~ ^(yes|y)$ ]]; then
   echo "Installing fzf...\n"
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
-  ~/.fzf/install &&
+  ~/.fzf/install
 fi
 
 read -r -p "Install ag? [y/N] " response
 response=${response,,}
 if [[ $response =~ ^(yes|y)$ ]]; then
   echo "Installing ag...\n"
-  apt-get install silversearcher-ag &&
+  apt-get install silversearcher-ag
 fi
 
 read -r -p "Install zsh? [y/N] " response
 response=${response,,}
 if [[ $response =~ ^(yes|y)$ ]]; then
   echo "Installing zsh...\n"
-  apt-get install zsh &&
+  apt-get install zsh
   echo "Installing zplug...\n"
-  curl -sL zplug.sh/installer | zsh &&
+  curl -sL zplug.sh/installer | zsh
   read -r -p "Make zsh default? [y/N] " response
   response=${response,,}
   if [[ $response =~ ^(yes|y)$ ]]; then
@@ -49,7 +49,7 @@ read -r -p "Install xclip? [y/N] " response
 response=${response,,}
 if [[ $response =~ ^(yes|y)$ ]]; then
   echo "Installing xclip...\n"
-  apt-get install xclip &&
+  apt-get install xclip
 fi
 
 echo "Creating symlinks...\n"
@@ -57,7 +57,7 @@ source createSymLink.sh
 
 echo "Installing vim-plug...\n"
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Installing vim plugins...\n"
 nvim +PlugInstall
