ddd0bebf9f6f84e9c10f3aaa7bd07240043d3a6f
arunlakshmanan <lakshma2@illinois.edu>
2017-10-21 22:49:13 -0500

    Add quick copy to fab

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
 from fabric.api import *
+import re
 
 CF_FIRMWARE = '~/code/cf-firmware'
+DOWNLOADS = '/tmp/dl'
 
 def mk(cload = 1):
     with lcd(CF_FIRMWARE):
@@ -8,6 +10,18 @@ def mk(cload = 1):
             local('make clean && make all && make cload')
         else:
             local('make clean && make all')
+
+def cpdl(n = 1):
+  """ Copy files from /tmp/dl """
+  for i in range(1, int(n)+1):
+    with lcd(DOWNLOADS):
+      out = local(('ls -dt -1 $PWD/** | head -%s | tail -1' % i), capture=True)
+    print(out)
+    with quiet():
+      if(out[-3:] == 'zip'):
+        local(('unzip -od . %s' % re.escape(out)))
+      else:
+        local(('cp %s .' % re.escape(out)))
 
 # TODO
 # 1. sshfs
